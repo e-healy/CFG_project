7e30d466246c661e6107a515a7efa6388fed2b2d
Ellie H <elliehealy20@outlook.com>
2024-02-13 16:03:32 +0000

    Add interactive Carousel button and javascript quiz for Seville webpage

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Scroll to top button
 const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
 btnScrollToTop.addEventListener("click", function () {
@@ -8,3 +9,126 @@ btnScrollToTop.addEventListener("click", function () {
     });
 
 });
+
+// Javascript Seville Quiz
+const questions = [
+    {
+        question: "Seville is the capital of Andalucia",
+        answers: [
+            {text: "True", correct: true},
+            {text: "False", correct: false},
+        ]
+    }, 
+    {
+        question: "Seville was a filming location for Game of Thrones and Star Wars",
+        answers: [
+            {text: "True", correct: true},
+            {text: "False", correct: false},
+        ]  
+    },
+    {
+        question: "Seville has four World Heritage Sites",
+        answers: [
+            {text: "True", correct: false},
+            {text: "False", correct: true},
+        ]
+    },
+    {
+        question: "Seville is the #1 city for having the most orange trees in the world",
+        answers: [
+            {text: "True", correct: true},
+            {text: "False", correct: false},
+        ]
+    },
+    {
+        question: "Seville is Europe's hottest city",
+        answers: [
+            {text: "True", correct: false},
+            {text: "False", correct: true},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("quiz-btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else {
+        startQuiz();
+    }
+})
+
+startQuiz()
+
